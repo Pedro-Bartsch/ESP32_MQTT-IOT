7ea7001701a3a1db729118d359df21aa94e05e88
Pedro-Bartsch <pedro_bartsch-silva@estudante.sesisenai.org.br>
2025-10-01 17:10:01 -0300

    criadas variaveis do MQTT, e função de conexão ao Wifi

--- MQTT_basic/MQTT_basic.ino
+++ MQTT_basic/MQTT_basic.ino
@@ -1,8 +1,40 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+const String SSID = "IPhone";
+const String PWSD = "iot_sul_123";
+
+const String brokerUrl = "";
+const int port = 1883;
+
+WiFiClient espClient;
+
+void connectLocalNetworks();
 
 void setup() {
-
+  Serial.begin(115200);
+  connectLocalNetworks();
 }
 
 void loop() {
+ // Detecta se o WiFi desconectou e tenta reconexão.
+  if (WiFi.status() != WL_CONNECTED){
+    Serial.println("Conexão perdida! Tentando reconexão.");
+    connectLocalNetworks();
+  }
+}
 
+void connectLocalNetworks(){
+  Serial.println("Iniciando conexão com rede WiFi");
+  WiFi.begin(SSID, PSWD);
+  
+  while (WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(200);
+  }
+  if (WiFi.status() == WL_CONNECTED){
+  Serial.println("\nConectado com sucesso!");
+  } else{
+    Serial.println("\nFalha ao conectar.");
+  }
 }
